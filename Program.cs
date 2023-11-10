696ae23cc33b059914308ab030352f63fe78a836
Alex Bragin <ab@alexbragin.com>
2023-11-10 14:46:05 +0300

    Add back comments

--- Program.cs
+++ Program.cs
@@ -4,6 +4,32 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
+/*
+
+Overview:
+    CsQuery is used to parse HTML content (TODO: Convert to HtmlParser use only)
+    UNUSED: ExCSS, AngleSharp code just for reference as CSS parsers
+    HtmlContent.LoadDocument() - main invocation of HTML layout and rendered are there
+    HtmlContent.GetDimensions() - hardcoded window size
+
+    BoxTree - lots of unimplemented unmarked stuff
+
+    CSS call chain:
+    class BoxTree
+    \/  BoxGetStyle() - stub
+    \/  nscssGetStyle() - stub
+    \/  CssSelectStyle() - currently implementing
+ 
+
+    Example of function pointers array: CssParser.ParseFunc
+
+    Now working on: class CssProps, implementing initial properties dispatcher for various needed props
+
+Useful links:
+    All possible array init syntices:
+        https://stackoverflow.com/questions/5678216/all-possible-array-initialization-syntaxes
+ */
+
 internal static class Program
 {
     /// <summary>
